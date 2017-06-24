3ee9bff730d7c92bd3faf2077427fc6b55a6cd6c
Belonias <iakovosbelonias@gmail.com>
2017-06-25 00:07:38 +0300

    001 problem

---
+++ 001/solution-001.py
@@ -0,0 +1,18 @@
+flag = False
+number = 232792559
+
+while flag is not True:
+    for divisor in range(2,21):
+        if number % divisor == 0:
+            flag = True
+        else:
+            flag = False
+            # I had an extra break
+            break
+
+    if flag == True:
+        print(number)
+        break
+
+    number += 1
+    # print(number)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Debug it!
+
+The purpose of this repository is for beginners to find easy examples to debug and hone their skill
+
+If you have code examples that you debugged please share with us
